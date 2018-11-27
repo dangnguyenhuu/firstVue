f8b6c32de8722a19a014342fa17e96bf9f224172
hikariraina <hikariraina@gmail.com>
2018-11-27 23:23:24 +0900

    add post editor

--- src/Pages/PageThreadShow.vue
+++ src/Pages/PageThreadShow.vue
@@ -2,30 +2,19 @@
 <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts" />
-    <form @submit.prevent="addPost">
-        <div class="form-group">
-            <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          class="form-input"
-          v-model="newPostText"
-        ></textarea>
-        </div>
-        <div class="form-actions">
-            <button class="btn-blue">Submit post</button>
-        </div>
-    </form>
+    <PostEditor @save="addPost" :threadId="id" />
 </div>
 </template>
 
 <script>
 import sourceData from '@/data'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
+
 export default {
     components: {
-        PostList
+        PostList,
+        PostEditor
     },
     props: {
         id: {
@@ -47,19 +36,11 @@ export default {
         }
     },
     methods: {
-        addPost() {
-            const postId = 'greatPost' + Math.random()
-            const post = {
-                text: this.newPostText,
-                publishedAt: Math.floor(Date.now() / 1000),
-                threadId: this.id,
-                userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
-                '.key': postId
-            }
+        addPost({post}) {
+            const postId = post['.key']
             this.$set(sourceData.posts, postId, post)
             this.$set(this.thread.posts, postId, postId)
             this.$set(sourceData.users[post.userId].posts, postId, postId)
-            this.newPostText = ''
         }
     }
 }
--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -12,15 +12,16 @@
             {{post.text}}
         </div>
     </div>
-    <div class="post-date text-faded">
-        {{post.publishedAt}}
+    <div class="post-date text-faded" :title="post.publishedAt | humanFriendlyDate">
+        {{post.publishedAt | diffForHumans}}
     </div>
 </div>
 </template>
 
- 
 <script>
 import sourceData from '@/data'
+import moment from 'moment'
+
 export default {
     props: {
         post: {
@@ -35,9 +36,17 @@ export default {
         userPostsCount() {
             return Object.keys(this.user.posts).length
         }
+    },
+    filters: {
+        humanFriendlyDate(date) {
+            return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+        },
+        diffForHumans(date) {
+            return moment.unix(date).fromNow()
+        }
     }
 }
 </script>
- 
+
 <style scoped>
  </style>
