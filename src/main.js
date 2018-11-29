bbdabfcb88a5990a6943bd57d33daeb9d742e590
hikariraina <hikariraina@gmail.com>
2018-11-29 22:51:42 +0900

    add forum

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import firebase from 'firebase'
+import AppDate from '@/components/AppDate'
 
+Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,17 @@ import Router from 'vue-router'
 import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
 import NotFound from '@/pages/PageNotFound'
+import Forum from '@/pages/PageForum'
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/thread/:id', name: 'ThreadShow', component: ThreadShow, props: true },
-    { path: '*', name: 'NotFound', component: NotFound }
-
+    { path: '/',               name: 'Home',            component: Home },
+    { path: '/forum/:id',      name: 'Forum',           component: Forum,         props: true},
+    { path: '/thread/:id',     name: 'ThreadShow',      component: ThreadShow,    props: true },
+    { path: '*',               name: 'NotFound',        component: NotFound }
   ],
   mode: 'history'
 })
