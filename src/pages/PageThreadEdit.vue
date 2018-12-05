19818c74ecdecfd525f5e403aaf09741d59ba06f
hikariraina <hikariraina@gmail.com>
2018-12-06 00:40:20 +0900

    fix bug editor

--- src/pages/PageThreadEdit.vue
+++ src/pages/PageThreadEdit.vue
@@ -36,21 +36,12 @@ export default {
                 title,
                 text
             }).then(thread => {
-                this.$router.push({
-                    name: 'ThreadShow',
-                    params: {
-                        id: thread['.key']
-                    }
-                })
+                this.$router.push({name: 'ThreadShow', params: {id: this.id}})
+
             })
         },
         cancel() {
-            this.$router.push({
-                name: 'Forum',
-                params: {
-                    id: this.forum['.key']
-                }
-            })
+            this.$router.push({name: 'ThreadShow', params: {id: this.id}})
         }
     }
 }
