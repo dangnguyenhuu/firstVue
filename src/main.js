b911b9439abb6fe3c64f0d1ca533618aa50ddb02
hikariraina <hikariraina@gmail.com>
2018-12-09 18:56:32 +0900

    add config

--- src/main.js
+++ src/main.js
@@ -6,20 +6,11 @@ import router from './router'
 import firebase from 'firebase'
 import AppDate from '@/components/AppDate'
 import store from '@/store'
-
+import {firebaseConfig} from './firebase-config'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
-
-var config = {
-  apiKey: "AIzaSyAXZ4b3Ux5yUO27jo-Z1hCMHobC8ZRECb8",
-  authDomain: "firstangular-adccc.firebaseapp.com",
-  databaseURL: "https://firstangular-adccc.firebaseio.com",
-  projectId: "firstangular-adccc",
-  storageBucket: "firstangular-adccc.appspot.com",
-  messagingSenderId: "289211327626"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,14 +1,17 @@
 <template>
-<div class="col-large push-top">
-    <h1>{{thread.title}}
-        <router-link :to="{name: 'ThreadEdit', id: this.id}" class="btn-green btn-small" tag="button">
-            Edit Thread
-        </router-link>
+<div v-if="thread && user" class="col-large push-top">
+    <h1>
+        {{thread.title}}
+        <router-link :to="{name: 'ThreadEdit', id: this.id}" class="btn-green btn-small" tag="button">Edit Thread</router-link>
     </h1>
     <p>
-        By <a href="#" class="link-unstyled">{{user.name}}</a>,
+        By
+        <a href="#" class="link-unstyled">{{user.name}}</a>,
         <AppDate :timestamp="thread.publishedAt" />.
-        <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">{{repliesCount}} replies by {{contributorsCount}} contributors</span>
+        <span
+        style="float:right; margin-top: 2px;"
+        class="hide-mobile text-faded text-small"
+      >{{repliesCount}} replies by {{contributorsCount}} contributors</span>
     </p>
     <PostList :posts="posts" />
     <PostEditor :threadId="id" />
@@ -16,9 +19,12 @@
 </template>
 
 <script>
-import PostList from '@/components/PostList'
-import PostEditor from '@/components/PostEditor'
-
+import firebase from "firebase";
+import PostList from "@/components/PostList";
+import PostEditor from "@/components/PostEditor";
+import {
+    countObjectProperties
+} from "@/utils";
 export default {
     components: {
         PostList,
@@ -30,32 +36,71 @@ export default {
             type: String
         }
     },
-
     computed: {
         thread() {
-            return this.$store.state.threads[this.id]
+            return this.$store.state.threads[this.id];
         },
         repliesCount() {
-            return this.$store.getters.threadRepliesCount(this.thread['.key'])
+            return this.$store.getters.threadRepliesCount(this.thread[".key"]);
         },
         user() {
-            return this.$store.state.users[this.thread.userId]
+            return this.$store.state.users[this.thread.userId];
         },
         contributorsCount() {
-            // find the replies
-            const replies = Object.keys(this.thread.posts)
-                .filter(postId => postId !== this.thread.firstPostId)
-                .map(postId => this.$store.state.posts[postId])
-            // get the user ids
-            const userIds = replies.map(post => post.userId)
-            // count the unique ids
-            return userIds.filter((item, index) => index === userIds.indexOf(item)).length
+            return countObjectProperties(this.thread.contributors);
         },
         posts() {
-            const postIds = Object.values(this.thread.posts)
-            return Object.values(this.$store.state.posts)
-                .filter(post => postIds.includes(post['.key']))
+            const postIds = Object.values(this.thread.posts);
+            return Object.values(this.$store.state.posts).filter(post =>
+                postIds.includes(post[".key"])
+            );
         }
+    },
+    created() {
+        // fetch thread
+        firebase.database().ref("threads").child(this.id).once("value", snapshot => {
+            const thread = snapshot.val();
+
+            console.log(thread);
+            this.$store.commit("setThread", {
+                threadId: snapshot.key,
+                thread: { ...thread,
+                    ".key": snapshot.key
+                }
+            });
+            // fetch user
+            firebase.database().ref("users").child(thread.userId).once("value", snapshot => {
+                const user = snapshot.val();
+                this.$store.commit("setUser", {
+                    userId: snapshot.key,
+                    user: { ...user,
+                        ".key": snapshot.key
+                    }
+                });
+            });
+            Object.keys(thread.posts).forEach(postId => {
+                // fetch post
+                firebase.database().ref("posts").child(postId).once("value", snapshot => {
+                    const post = snapshot.val();
+                    this.$store.commit("setPost", {
+                        postId: snapshot.key,
+                        post: { ...post,
+                            ".key": snapshot.key
+                        }
+                    });
+                    // fetch user
+                    firebase.database().ref("users").child(post.userId).once("value", snapshot => {
+                        const user = snapshot.val();
+                        this.$store.commit("setUser", {
+                            userId: snapshot.key,
+                            user: { ...user,
+                                ".key": snapshot.key
+                            }
+                        });
+                    });
+                });
+            });
+        });
     }
-}
+};
 </script>
