1612a0ec0d65f2b349349aa5e1b0efda3b86e392
hikariraina <hikariraina@gmail.com>
2018-12-09 22:49:37 +0900

    fix gitignore

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDqSsysc8kkUmNDQzHF6n-mzKINwdK7BjA",
+    authDomain: "testchatapp-1ef27.firebaseapp.com",
+    databaseURL: "https://testchatapp-1ef27.firebaseio.com",
+    projectId: "testchatapp-1ef27",
+    storageBucket: "testchatapp-1ef27.appspot.com",
+    messagingSenderId: "21097998397"
+  }
+  
