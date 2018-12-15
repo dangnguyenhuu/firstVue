c2ee12d94b2a2333dbfb775dac016e4f874185d4
hikariraina <hikariraina@gmail.com>
2018-12-15 15:14:27 +0900

    config using dotenv

--- src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_ID
-  }
-  
--- src/main.js
+++ src/main.js
@@ -6,11 +6,20 @@ import router from './router'
 import firebase from 'firebase'
 import AppDate from '@/components/AppDate'
 import store from '@/store'
-import {firebaseConfig} from './firebase-config'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
-firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_ID
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
