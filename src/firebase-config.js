b3bd2b8c38010fc8a54c178a6334e6e17b6ee1ef
hikariraina <hikariraina@gmail.com>
2018-12-15 14:28:31 +0900

    refactoring config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDqSsysc8kkUmNDQzHF6n-mzKINwdK7BjA",
-    authDomain: "testchatapp-1ef27.firebaseapp.com",
-    databaseURL: "https://testchatapp-1ef27.firebaseio.com",
-    projectId: "testchatapp-1ef27",
-    storageBucket: "testchatapp-1ef27.appspot.com",
-    messagingSenderId: "21097998397"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_ID
   }
   
