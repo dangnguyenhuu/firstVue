1dcebdaadb1486e86b7da79536e62fd69f0f8d5a
hikariraina <hikariraina@gmail.com>
2018-12-18 23:34:39 +0900

    signin with gmail

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -28,9 +28,7 @@
         </div>
       </form>
       <div class="text-center push-top">
-        <button class="btn-red btn-xsmall">
-          <i class="fa fa-google fa-btn"></i>Sign up with Google
-        </button>
+        <button @click="registerWithGoogle" class="btn-red btn-xsmall"><i class="fa fa-google fa-btn"></i>Sign up with Google</button>
       </div>
     </div>
   </div>
@@ -54,6 +52,10 @@ export default {
       this.$store
         .dispatch("registerUserWithEmailAndPassword", this.form)
         .then(() => this.$router.push("/"));
+    },
+    registerWithGoogle () {
+        this.$store.dispatch('signInWithGoogle')
+            .then(() => this.$router.push('/'))
     }
   },
   created() {
--- src/pages/PageSignIn.vue
+++ src/pages/PageSignIn.vue
@@ -24,36 +24,41 @@
       </form>
 
       <div class="push-top text-center">
-        <button class="btn-red btn-xsmall"><i class="fa fa-google fa-btn"></i>Sign in with Google</button>
+        <button @click="signInWithGoogle" class="btn-red btn-xsmall"><i class="fa fa-google fa-btn"></i>Sign in with Google</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-      data () {
+export default {
+    data() {
         return {
-          form: {
-            email: null,
-            password: null
-          }
+            form: {
+                email: null,
+                password: null
+            }
         }
-      },
-      methods: {
-        signIn () {
-          this.$store.dispatch('signInWithEmailAndPassword', {
-            email: this.form.email,
-            password: this.form.password
-          })
+    },
+    methods: {
+        signIn() {
+            this.$store.dispatch('signInWithEmailAndPassword', {
+                    email: this.form.email,
+                    password: this.form.password
+                })
+                .then(() => this.$router.push('/'))
+                .catch(error => alert('🤷‍️' + error.message))
+        },
+        signInWithGoogle () {
+          this.$store.dispatch('signInWithGoogle')
             .then(() => this.$router.push('/'))
             .catch(error => alert('🤷‍️' + error.message))
         }
-      },
-      created () {
+    },
+    created() {
         this.$emit('ready')
-      }
     }
+}
 </script>
 
 <style scoped>
